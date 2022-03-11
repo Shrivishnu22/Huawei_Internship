2f168899bebed718302e756d0cd5464c13decdb9
Shri Vishnu <62836567+Shrivishnu22@users.noreply.github.com>
2022-03-11 13:36:29 +0530

    Create studentDB.sql

---
+++ Assignment 1/studentDB.sql
@@ -0,0 +1,18 @@
+//Creation of a table 'Student'
+CREATE TABLE student(id integer PRIMARY Key AUTOINCREMENT,name varchar(20),age integer, mobile_no integer, reg_no integer, batch integer);
+
+//Creation of a table 'Teacher'
+CREATE TABLE teacher(id integer PRIMARY Key AUTOINCREMENT,name varchar(20),domain varchar(60),department varchar(20));
+
+
+//Updation of values in table 'Student'
+INSERT into student VALUES (01,"Vimal",20,1234567890,811719104118,2023);
+INSERT into student VALUES (2,"Arun",20,5686266887,811719104058,2023);
+INSERT into student VALUES (3,"Rooban",20,2681264484,811719104022,2023);
+INSERT into student VALUES (4,"Ronaldo",20,2681264484,811719104022,2023);
+INSERT into student VALUES (5,"Ram",20,2681264484,811719104022,2023);
+INSERT into student VALUES (6,"Rakesh",20,2681264484,811719104022,2023);
+INSERT into student VALUES (7,"Thulasi",20,2681264484,811719104022,2023);
+INSERT into student VALUES (8,"Sanjay",20,2681264484,811719104022,2023);
+INSERT into student VALUES (9,"Shyam",20,2681264484,811719104022,2023);
+INSERT into student VALUES (10,"Nesainth",20,2681264484,811719104022,2023);
